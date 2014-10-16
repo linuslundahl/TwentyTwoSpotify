287f0057d6b204087bd8bbe89931b4a090974d26
Linus Lundahl <lundahl.linus@gmail.com>
2014-10-16 13:47:19 +0200

    Cleanup gulp-file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 var gulp = require('gulp'),
     $    = require('gulp-load-plugins')();
 
-var handleError = function (err) {
-  $.notify.onError()(err);
-  this.emit('end');
-};
-
 // Default
 gulp.task('default', function () {
   gulp.watch('TwentyTwoSpotify.js', ['scripts']);
